750f6fa968fb67a7e020bb928dba4d687d513a60
Amarendra Sahoo <amarendrait@gmail.com>
2020-04-09 14:57:05 +0530

    Added rating sorter

--- src/main/java/com/westernacher/internal/feedback/controller/RatingScaleController.java
+++ src/main/java/com/westernacher/internal/feedback/controller/RatingScaleController.java
@@ -1,8 +1,6 @@
 package com.westernacher.internal.feedback.controller;
 
-import com.westernacher.internal.feedback.domain.GoalDefinition;
 import com.westernacher.internal.feedback.domain.RatingScale;
-import com.westernacher.internal.feedback.repository.GoalDefinitionRepository;
 import com.westernacher.internal.feedback.repository.RatingScaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -21,12 +21,30 @@ public class RatingScaleController {
 
     @RequestMapping(method = RequestMethod.GET)
     public List<RatingScale> getAll () {
-        return repository.findAll();
+
+        List<RatingScale> ratingScales = repository.findAll();
+
+        Collections.sort(ratingScales, new RatingSorter());
+
+        return ratingScales;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public void saveRatingScale (@RequestBody List<RatingScale> ratingScales) {
         repository.saveAll(ratingScales);
+    }
+}
+
+class RatingSorter implements Comparator<RatingScale> {
+    @Override
+    public int compare(RatingScale o1, RatingScale o2) {
+        return extractInt(o1.getRating()) - extractInt(o2.getRating());
+    }
+
+    int extractInt(String s) {
+        String num = s.replaceAll("\\D", "");
+        // return 0 if no digits found
+        return num.isEmpty() ? 0 : Integer.parseInt(num);
     }
 }
 
