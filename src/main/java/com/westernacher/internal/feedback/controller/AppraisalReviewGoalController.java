05418d0f513b66d5292b7fef9cf2e3e579d5a7d6
Amarendra Sahoo <amarendrait@gmail.com>
2020-09-17 11:47:50 +0530

    Refactor

--- src/main/java/com/westernacher/internal/feedback/controller/AppraisalReviewGoalController.java
+++ src/main/java/com/westernacher/internal/feedback/controller/AppraisalReviewGoalController.java
@@ -1,10 +1,13 @@
 
 package com.westernacher.internal.feedback.controller;
 
+import com.westernacher.internal.feedback.domain.AppraisalReview;
 import com.westernacher.internal.feedback.domain.AppraisalReviewGoal;
 import com.westernacher.internal.feedback.repository.AppraisalReviewGoalRepository;
+import com.westernacher.internal.feedback.repository.AppraisalReviewRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +22,9 @@ public class AppraisalReviewGoalController {
 
     @Autowired
     private AppraisalReviewGoalRepository repository;
+
+    @Autowired
+    private AppraisalReviewRepository reviewRepository;
 
     @GetMapping
     public ResponseEntity<?> getReviewGoals (@RequestParam String appraisalId) {
@@ -47,5 +53,48 @@ public class AppraisalReviewGoalController {
         });
 
         return ResponseEntity.ok(newReviewGoals);
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<?> submit (@RequestBody List<AppraisalReviewGoal> reviewGoals) {
+        List<AppraisalReviewGoal> newReviewGoals = new ArrayList<>();
+
+        boolean submit = true;
+        String appraisalReviewId = null;
+        List<String> sectionOneError = new ArrayList<>();
+        ErrorResource errorResource = new ErrorResource();
+
+        for (AppraisalReviewGoal appraisalReviewGoal : reviewGoals) {
+
+            if (appraisalReviewGoal.getComment() == null) {
+                sectionOneError.add("Null Comment");
+                submit = false;
+            } else if (appraisalReviewGoal.getComment().length()  < 5) {
+                sectionOneError.add("Comment length is not less than five");
+                submit = false;
+            }
+
+            if (appraisalReviewGoal.getRating() == null) {
+                sectionOneError.add("Rating is null");
+                submit = false;
+            }
+
+            if (submit == false)  {
+                errorResource.setSectionOneError(sectionOneError);
+                return new ResponseEntity<ErrorResource>(errorResource, HttpStatus.NOT_ACCEPTABLE);
+            } else {
+                appraisalReviewGoal.setComplete(true);
+                newReviewGoals.add(appraisalReviewGoal);
+            }
+            appraisalReviewId = appraisalReviewGoal.getAppraisalId();
+        }
+
+        if (submit = true) {
+            //AppraisalReview appraisalReview = re
+
+            //if ()
+            return ResponseEntity.ok(repository.saveAll(newReviewGoals));
+        }
+        return null;
     }
 }
