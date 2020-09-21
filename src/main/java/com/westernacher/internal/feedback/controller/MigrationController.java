6451c7d01a8a357687a11f04d4f1be30cb22951d
Amarendra Sahoo <amarendrait@gmail.com>
2020-09-21 16:01:42 +0530

    Added migration

---
+++ src/main/java/com/westernacher/internal/feedback/controller/MigrationController.java
@@ -0,0 +1,29 @@
+package com.westernacher.internal.feedback.controller;
+
+import com.westernacher.internal.feedback.domain.Appraisal;
+import com.westernacher.internal.feedback.domain.Goal;
+import com.westernacher.internal.feedback.repository.GoalRepository;
+import com.westernacher.internal.feedback.service.GoalService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/migrate")
+public class MigrationController {
+
+    @PostMapping
+    public ResponseEntity<?> migrateAppraisal (@RequestBody List<Appraisal> appraisals) {
+
+        return ResponseEntity.ok(appraisals);
+    }
+}
+
+
