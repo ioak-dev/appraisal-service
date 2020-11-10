62c28d4ec0604c33e2b82fd97e26aa9188254906
Amarendra Sahoo <amarendrait@gmail.com>
2020-11-10 13:02:34 +0530

    refactor

--- src/main/java/com/westernacher/internal/feedback/jwt/JwtTokenUtil.java
+++ src/main/java/com/westernacher/internal/feedback/jwt/JwtTokenUtil.java
@@ -9,6 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
 import java.util.function.Function;
 
@@ -46,11 +51,35 @@ public class JwtTokenUtil {
     private Claims extractAllClaims(String token) {
         Jws<Claims> claimsJws = null;
         try {
-            claimsJws = Jwts.parser().setSigningKey("secret".getBytes("UTF-8")).parseClaimsJws(token);
+            claimsJws = Jwts.parser().setSigningKey(getParsedPublicKey().get()).parseClaimsJws(token);
         }catch (Exception e) {
             e.printStackTrace();
         }
         return claimsJws.getBody();
+    }
+
+    public static Optional<RSAPublicKey> getParsedPublicKey() {
+        // public key content...excluding '---PUBLIC KEY---' and '---END PUBLIC KEY---'
+        String PUB_KEY = "secret";
+
+        // removes white spaces or char 20
+        String PUBLIC_KEY = "";
+        if (!PUB_KEY.isEmpty()) {
+            PUBLIC_KEY = PUB_KEY.replace(" ", "");
+        }
+
+        try {
+            byte[] decode = com.google.api.client.util.Base64.decodeBase64(PUBLIC_KEY);
+            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(decode);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPublicKey pubKey = (RSAPublicKey) keyFactory.generatePublic(keySpecX509);
+            return Optional.of(pubKey);
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            System.out.println("Exception block | Public key parsing error ");
+            return Optional.empty();
+        }
     }
 
     public Boolean isTokenExpired(String token) {
