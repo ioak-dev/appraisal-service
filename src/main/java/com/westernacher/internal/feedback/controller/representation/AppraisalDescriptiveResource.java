aa5e6cfc5cf742fa030960cc0a8614ccdfe6802b
Amarendra Sahoo <amarendra.sahoo@westernacher.com>
2021-06-15 19:29:52 +0530

    refactor

---
+++ src/main/java/com/westernacher/internal/feedback/controller/representation/AppraisalDescriptiveResource.java
@@ -0,0 +1,44 @@
+package com.westernacher.internal.feedback.controller.representation;
+
+import com.westernacher.internal.feedback.domain.v2.AppraisalDescriptive;
+import com.westernacher.internal.feedback.domain.v2.AppraisalHeader;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@Builder
+public class AppraisalDescriptiveResource {
+    private String id;
+    private Integer from;
+    private Integer to;
+    private String employeeId;
+    private String reviewerId;
+    private String reviewerType;
+    private String start;
+    private String stop;
+    private String toContinue;
+
+
+    public static AppraisalDescriptiveResource getAppraisalDescriptiveResource(AppraisalDescriptive appraisalDescriptive,
+                                                                        Map<String, AppraisalHeader> appraisalHeaderMap) {
+
+        AppraisalHeader appraisalHeader = appraisalHeaderMap.get(appraisalDescriptive.getHeaderId());
+        if (appraisalHeader != null && appraisalHeader.getId() != null) {
+            return AppraisalDescriptiveResource.builder()
+                    .id(appraisalDescriptive.getId())
+                    .from(appraisalHeader.getFrom())
+                    .to(appraisalHeader.getTo())
+                    .employeeId(appraisalHeader.getEmployeeId())
+                    .reviewerId(appraisalHeader.getReviewerId())
+                    .reviewerType(appraisalHeader.getReviewerType())
+                    .start(appraisalDescriptive.getStart())
+                    .stop(appraisalDescriptive.getStop())
+                    .toContinue(appraisalDescriptive.getToContinue())
+                    .build();
+        }
+       return null;
+
+    }
+}
--- src/main/java/com/westernacher/internal/feedback/controller/v2/AppraisalDescriptiveController.java
+++ src/main/java/com/westernacher/internal/feedback/controller/v2/AppraisalDescriptiveController.java
@@ -2,9 +2,13 @@
 package com.westernacher.internal.feedback.controller.v2;
 
 
+import com.westernacher.internal.feedback.controller.representation.AppraisalDescriptiveResource;
+import com.westernacher.internal.feedback.controller.representation.AppraisalLongResource;
 import com.westernacher.internal.feedback.domain.v2.AppraisalDescriptive;
+import com.westernacher.internal.feedback.domain.v2.AppraisalHeader;
 import com.westernacher.internal.feedback.domain.v2.AppraisalLong;
 import com.westernacher.internal.feedback.repository.v2.AppraisalDescriptiveRepository;
+import com.westernacher.internal.feedback.repository.v2.AppraisalHeaderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/descriptive")
@@ -22,12 +28,37 @@ public class AppraisalDescriptiveController {
     @Autowired
     private AppraisalDescriptiveRepository repository;
 
+    @Autowired
+    private AppraisalHeaderRepository appraisalHeaderRepository;
+
     @GetMapping
-    public ResponseEntity<List<AppraisalDescriptive>> getAll (@RequestParam(required = false) String headerId) {
+    public ResponseEntity<List<AppraisalDescriptiveResource>> getAll (@RequestParam(required = false) String headerId) {
+
+        Map<String, AppraisalHeader> appraisalHeaderMap = new HashMap<>();
+        appraisalHeaderRepository.findAll().stream().forEach(appraisalHeader -> {
+            appraisalHeaderMap.put(appraisalHeader.getId(), appraisalHeader);
+        });
+
+        List<AppraisalDescriptive> headers = new ArrayList<>();
+
         if (headerId != null) {
-            return ResponseEntity.ok(repository.findAllByHeaderId(headerId));
+            headers = repository.findAllByHeaderId(headerId);
+        } else {
+            headers = repository.findAll();
         }
-        return ResponseEntity.ok(repository.findAll());
+
+        List<AppraisalDescriptiveResource> resources = new ArrayList<>();
+
+        headers.stream().forEach(header->{
+            resources.add(AppraisalDescriptiveResource.getAppraisalDescriptiveResource(header, appraisalHeaderMap));
+        });
+
+        /*resources.stream()
+                .sorted(Comparator.comparing(AppraisalDescriptiveResource::getReviewerType)
+        .thenComparing(AppraisalDescriptiveResource::getReviewerId))
+                .collect(Collectors.toList());*/
+
+        return ResponseEntity.ok(resources);
     }
 
 
